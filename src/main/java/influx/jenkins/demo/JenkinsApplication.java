f7ca2493db32569101974e0dab8bcaeef4d9e332
influx.pine <influx.pine@gmail.com>
2020-01-04 14:59:14 +0900

    web port add and test response

--- src/main/java/influx/jenkins/demo/JenkinsApplication.java
+++ src/main/java/influx/jenkins/demo/JenkinsApplication.java
@@ -3,6 +3,7 @@ package influx.jenkins.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class JenkinsApplication {
 
---
+++ src/main/java/influx/jenkins/demo/controller/DefaultController.java
@@ -0,0 +1,28 @@
+package influx.jenkins.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+
+
+@RestController
+public class DefaultController {
+
+    @GetMapping("/home")
+    public String home2(){
+        return "hello world";
+    }
+
+
+    @GetMapping("/")
+    public String home(){
+        return "this is home";
+    }
+
+
+    @PostConstruct
+    void init(){
+        System.out.println("this bean is load ");
+    }
+}
